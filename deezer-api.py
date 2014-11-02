76a3ea373e2f522c8f5d621d5b72b181ff8bb6bc
Kaefertal <Kaefertal@users.noreply.github.com>
2014-11-02 18:39:20 +0100

    Update deezer-api.py
    Added some comments and removed an unneccessary variable. Will comment more later.

--- deezer-api.py
+++ deezer-api.py
@@ -2,19 +2,25 @@ import json
 import requests
 import csv
 
-
-index = 0
+#Create empty lists to be filled later.
 artists = []
 tracks = []
+
+#Read the API-Token and User-ID from the respective file.
 token = open("token", "r", encoding = "utf-8").read().rstrip()
 user_id = open("user_id", "r", encoding = "utf-8").read().rstrip()
+
+#Get your artists and tracks in JSON-format.
 url_artists = "http://api.deezer.com/user/" + user_id + "/artists?access_token=" + token + "&output=json"
 url_tracks = "http://api.deezer.com/user/" + user_id + "/tracks?access_token=" + token + "&output=json"
+
+#Create variables.
 csv_artists = "artists.csv"
 csv_tracks = "tracks.csv"
 
 
-def get_artists(index, artists, url):
+#Function for fetching artists and writing them in the list "artists".
+def get_artists(artists, url):
 
     while True:
         r = requests.get(url)
@@ -35,7 +41,8 @@ def get_artists(index, artists, url):
             break
 
 
-def get_tracks(index, tracks, url):
+#Function for fetching tracks and writing them in the list "tracks".
+def get_tracks(tracks, url):
     while True:
         r = requests.get(url)
         deezer = r.content;
@@ -57,6 +64,7 @@ def get_tracks(index, tracks, url):
         break
 
 
+#Writing artists in the CSV-file "artists.csv".
 def write_artists():
     with open(csv_artists, "w") as output:
         writer = csv.writer(output, lineterminator='\n')
@@ -65,6 +73,7 @@ def write_artists():
             writer.writerow([val])
     print("Artists successfully written in CSV-file.")
 
+#Writing tracks in the CSV-file "tracks.csv".
 def write_tracks():
     with open(csv_tracks, "w") as output:
         writer = csv.writer(output, lineterminator='\n')
@@ -74,7 +83,8 @@ def write_tracks():
     print("Tracks written successfully in CSV-file.")
 
 
-get_artists(index, artists, url_artists)
+#Calling all the functions one after another.
+get_artists(artists, url_artists)
 write_artists()
-get_tracks(index, tracks, url_tracks)
+get_tracks(tracks, url_tracks)
 write_tracks()
